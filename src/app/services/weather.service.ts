ec19094dc9b45ea5d1aa1f08bcf479e0f64431f1
David Ben Ishai <benditi@gmail.com>
2021-12-23 19:00:57 +0200

    changed second call to https

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -8,7 +8,7 @@ export class WeatherService {
 
   constructor(private http: HttpClient) { }
   public async findWeather(cityKey:string){
-    const forecast = await this.http.get<any>(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=0UCIQbymKHASO6CCAmFCmdx0EnWufncK&details=true&metric=true`)
+    const forecast = await this.http.get<any>(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=0UCIQbymKHASO6CCAmFCmdx0EnWufncK&details=true&metric=true`)
     return forecast
   }
 }
