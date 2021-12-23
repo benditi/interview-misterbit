9b1b2e63be5230ef5d8dd84bf360eb9062917b7b
David Ben Ishai <benditi@gmail.com>
2021-12-23 18:52:40 +0200

    changed API call to https

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -9,7 +9,7 @@ export class LocationService {
 
   constructor(private http: HttpClient) { }
   public async getCityAutocomplete(cityName: string) {
-    const optionalCities = await this.http.get<any>(`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=0UCIQbymKHASO6CCAmFCmdx0EnWufncK&q=${cityName}`)
+    const optionalCities = await this.http.get<any>(`https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=0UCIQbymKHASO6CCAmFCmdx0EnWufncK&q=${cityName}`)
     return optionalCities;
   }
 
